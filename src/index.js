6dae08e15743093047512b0f97b5b1ed64ee494e
Kadamir24 <amir.kaniyev@gmail.com>
2020-10-09 12:50:21 +0600

    finish brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let array = str.split('');
+  for (let i = 0; i < array.length; i++) {
+    let brCon = bracketsConfig.filter( (elem) => elem.includes(array[i]))
+    //console.log(brCon)
+    let left = brCon[0][0];
+    let right = brCon[0][1];
+    if (left == right) {
+      if (stack[stack.length - 1] == array[i]) {
+        stack.pop();
+      } else {
+        stack.push(array[i]);
+      }
+    } else {
+      if (array[i] == left) {
+          stack.push(array[i])
+          continue
+      } else if (array[i] == right) {
+         if (stack[stack.length -1] != left) {
+            return false
+          }
+        stack.pop()
+      }
+    }
+  }
+  if (stack.length == 0) {
+    return true
+  } else {
+    return false
+  }
 }
